ac80305ed06670aef213c65646a02d2fe87ad9c1
dmitryreaper <dimadimof81@gmail.com>
2025-01-30 20:52:35 +0000

    add: starting system

--- src/main.asm
+++ src/main.asm
@@ -1,18 +1,54 @@
 	org 0x7C00
 	bits 16
 
-main:
 
+%DEFINE	ENDL 0x0D, 0x0A	
+
+start:
+	jmp main
+
+	;; print a string to screen.
+	;; Params:
+	;; - ds, si points to string
+puts:
+	;; save registers we will modify
+	push si
+	push ax
+
+.loop:
+	lodsb						; load next character in al
+	or al,	al					; verivy if next character is null
+	jz .done
+
+	mov ah, 0x0e 				; call bios interrupt
+	int 0x10
+	jmp .loop
+
+.done:
+	pop ax
+	pop si
+	ret
+	
+main:
 	;; setup data segments
 	mov ax, 0					; can't write to ds/es directly
 	mov ds, ax
 	mov es, ax
 
 	;; setup stack
+	mov ss, ax
+	mov sp, 0x7C00				; stack grows downwards from where we are loaded in memory
+
+	;; pring message
+	mov	si, msg_hello
+	call puts
+	
 	hlt
 	
 .halt:
 	jmp .halt
+
+msg_hello: db 'Start system', ENDL, 0
 	
 times 510-($-$$) db 0
 dw 0AA55h
